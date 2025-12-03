aa04ca9697ad93353dadee02b3180e64c5379aca
Diya Gangal <gangaldiya679@gmail.com>
2025-12-03 20:42:53 +0530

    Add files via upload

---
+++ guessnumber.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int secret, guess, attempts = 0;
+
+    // initialize random number generator
+    srand(time(NULL));
+    secret = rand() % 100 + 1;  // number between 1 and 100
+
+    printf("Welcome to Number Guessing Game!\n");
+    printf("I have chosen a number between 1 and 100.\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess < secret) {
+            printf("Too low! Try again.\n");
+        } else if (guess > secret) {
+            printf("Too high! Try again.\n");
+        } else {
+            printf("Correct! You guessed it in %d attempts.\n", attempts);
+        }
+
+    } while (guess != secret);
+
+    return 0;
+}
